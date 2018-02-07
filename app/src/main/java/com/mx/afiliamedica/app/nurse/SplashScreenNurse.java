8f32968675a1548ce19e9a18b217895b823ef42b
AlanMartinezAS <35229498+JorgeMartinezAS@users.noreply.github.com>
2018-02-06 18:20:39 -0600

    Initial commit

---
+++ app/src/main/java/com/mx/afiliamedica/app/nurse/SplashScreenNurse.java
@@ -0,0 +1,13 @@
+package com.mx.afiliamedica.app.nurse;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreenNurse extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen_nurse);
+    }
+}
